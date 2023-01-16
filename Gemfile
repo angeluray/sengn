7415660f2e1345e3dc6f47437f36f1dfd60de237
angeluray <aeucampos@gmail.com>
2023-01-16 10:17:32 -0400

    Add tailwind

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "tailwindcss-rails", "~> 2.0"
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :searches
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+  resources :users do
+    resources :searches
+  end
   # Defines the root path route ("/")
   root "searches#index"
 end
